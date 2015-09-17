9886111f558889c3f2ee3f0f0340e37af688d664
Bardur Arantsson <bardur@scientician.net>
2015-09-17 21:47:25 +0200

    Add script to delete sandboxes

---
+++ clean-sandboxes.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -xe
+D=$(dirname $0)
+
+# Make sure we're running in the project's root directory
+cd "${D}"
+
+# Remove all sandboxes that have potentially been created by the
+# build-*.sh scripts
+for d in "${D}" "${D}/cqrs-core" "${D}/cqrs-testkit" "${D}/cqrs-memory" "${D}/cqrs-postgresql" "${D}/cqrs-example"; do
+  (cd "${d}" && cabal sandbox delete)
+done

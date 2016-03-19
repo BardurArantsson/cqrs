6797629d8e4bee8db147de6a2b0af55f2da4efd2
Bardur Arantsson <bardur@scientician.net>
2016-03-19 08:35:19 +0100

    cqrs-postgresql: Use UPSERT for SnapshotStore

--- cqrs-postgresql/src/Data/CQRS/PostgreSQL/Internal/SnapshotStore.hs
+++ cqrs-postgresql/src/Data/CQRS/PostgreSQL/Internal/SnapshotStore.hs
@@ -3,7 +3,6 @@ module Data.CQRS.PostgreSQL.Internal.SnapshotStore
     ( newSnapshotStore
     ) where
 
-import           Control.Exception (catchJust)
 import           Control.Monad.IO.Class (liftIO)
 import           Data.ByteString (ByteString)
 import           Data.CQRS.Types.Snapshot (Snapshot(..))
@@ -18,48 +17,27 @@ import qualified System.IO.Streams as Streams
 
 writeSnapshot :: Pool Connection -> Tables -> ByteString -> Snapshot ByteString -> IO ()
 writeSnapshot connectionPool tables aggregateId (Snapshot v d) =
-  -- We ignore duplicate key exceptions since snapshots aren't
-  -- important enough to merit aborting the user's command. We'll
-  -- probably have opportunities to write a snapshot for the
-  -- aggregate later anyway.
-  ignoreDuplicateKey $
-    runTransactionP connectionPool $ do
-      maybeUpdatedRows <- execSql' updateSnapshotSql
-        [ SqlByteArray (Just d)
-        , SqlInt32 $ Just $ fromIntegral v
-        , SqlByteArray $ Just aggregateId
-        ]
-      if (maybe 0 id maybeUpdatedRows) > 0 then
-        return () -- Update happened, done!
-        else
-          -- Not updated; this means that the snapshot row didn't exist,
-          -- so we'll try to INSERT it now.
-          execSql insertSnapshotSql
-            [ SqlByteArray $ Just aggregateId
-            , SqlByteArray (Just d)
-            , SqlInt32 $ Just $ fromIntegral v
-            ]
-
+  -- We ignore the possibility of data races with others trying to
+  -- update the same snapshot since snapshots aren't important enough
+  -- to merit the extra complexity.
+  runTransactionP connectionPool $ do
+    execSql upsertSnapshotSql
+      [ SqlByteArray $ Just aggregateId
+      , SqlByteArray (Just d)
+      , SqlInt32 $ Just $ fromIntegral v
+      ]
   where
     snapshotTable = tblSnapshot tables
 
-    updateSnapshotSql = [text|
-      UPDATE $snapshotTable
-         SET "data" = $$1
-           , "version" = $$2
-       WHERE "aggregate_id" = $$3
-    |]
-
-    insertSnapshotSql = [text|
+    upsertSnapshotSql = [text|
       INSERT INTO $snapshotTable
                   ("aggregate_id", "data", "version")
            VALUES ($$1, $$2, $$3)
+      ON CONFLICT ("aggregate_id")
+        DO UPDATE
+              SET "data" = $$2
+                , "version" = $$3
     |]
-
-    -- Run an action, ignoring duplicate key exceptions.
-    ignoreDuplicateKey action =
-      catchJust isDuplicateKey action $ \_ ->
-        return () -- Ignore
 
 readSnapshot :: Pool Connection -> Tables -> ByteString -> IO (Maybe (Snapshot ByteString))
 readSnapshot connectionPool tables aggregateId = do

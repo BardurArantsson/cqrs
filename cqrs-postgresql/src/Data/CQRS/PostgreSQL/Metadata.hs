8d6c5b13af7751d094213a9735706718c83bf2db
Bardur Arantsson <bardur@scientician.net>
2016-10-15 08:36:33 +0200

    Generalize 'toSQL' for PostgreSQL metadata

--- cqrs-postgresql/src/Data/CQRS/PostgreSQL/Metadata.hs
+++ cqrs-postgresql/src/Data/CQRS/PostgreSQL/Metadata.hs
@@ -16,6 +16,11 @@ data Schema = DefaultSchema
 -- | Table name, including which schema it is in.
 data Table = Table Schema Text
 
+-- | Convert metadata object identifier to its quoted SQL
+-- representation.
+class ToSQL a where
+    toSQL :: a -> Text
+
 -- | Quote a PostgreSQL object identifier. Useful in circumstances
 -- where you need to quote a dynamically generated identifier.
 quoteToSQL :: Text -> Text
@@ -27,9 +32,14 @@ quoteToSQL i = T.concat [ singleQt
     singleQt = "\""
     doubleQt = "\"\""
 
--- | Turn a 'Table' into a fully-qualified SQL string.
-toSQL :: Table -> Text
-toSQL (Table DefaultSchema tableId) =
-    quoteToSQL tableId
-toSQL (Table (NamedSchema schemaId) tableId) =
-    T.concat [quoteToSQL schemaId, ".", quoteToSQL tableId]
+--
+-- Instances
+--
+
+instance ToSQL Schema where
+  toSQL DefaultSchema = quoteToSQL "public"
+  toSQL (NamedSchema schemaId) = quoteToSQL schemaId
+
+instance ToSQL Table where
+  toSQL (Table schema tableId) =
+    T.concat [toSQL schema, ".", quoteToSQL tableId]

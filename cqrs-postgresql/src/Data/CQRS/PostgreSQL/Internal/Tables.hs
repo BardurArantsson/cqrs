6ec541496171adc44de2d7ad2338e1d050148f25
Bardur Arantsson <bardur@scientician.net>
2017-03-24 19:42:16 +0100

    Add Haddock comments

--- cqrs-postgresql/src/Data/CQRS/PostgreSQL/Internal/Tables.hs
+++ cqrs-postgresql/src/Data/CQRS/PostgreSQL/Internal/Tables.hs
@@ -1,16 +1,18 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Data.CQRS.PostgreSQL.Internal.Tables
-    ( mkTables
-    , Tables(..)
+    ( Tables(..)
+    , mkTables
     ) where
 
 import           Data.CQRS.PostgreSQL.Metadata
 import           Data.Text (Text)
 
+-- | Core tables.
 data Tables = Tables { tblEvent :: Text
                      , tblSnapshot :: Text
                      }
 
+-- | Create core tables for a given schema.
 mkTables :: Schema -> Tables
 mkTables schema = Tables { tblEvent = toSQL $ Table schema "event"
                          , tblSnapshot = toSQL $ Table schema "snapshot"

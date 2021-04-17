d3c01f16d6bd70e4f373b01818177dafe98e2b48
Bardur Arantsson <bardur@scientician.net>
2021-04-17 09:24:58 +0200

    Add StorageBackend

---
+++ cqrs-core/src/Data/CQRS/Types/StorageBackend.hs
@@ -0,0 +1,38 @@
+module Data.CQRS.Types.StorageBackend
+  ( StorageBackend(..)
+  , transformI
+  , transformE
+  , transformA
+  ) where
+
+import           Data.CQRS.Types.EventStore (EventStore)
+import qualified Data.CQRS.Types.EventStore as ES
+import           Data.CQRS.Types.Iso (Iso)
+import           Data.CQRS.Types.SnapshotStore (SnapshotStore)
+import qualified Data.CQRS.Types.SnapshotStore as SS
+
+data StorageBackend a i e = StorageBackend {
+    -- | Event store.
+    sbEventStore :: EventStore i e
+  ,
+    -- | Snapshot store.
+    sbSnapshotStore :: SnapshotStore i a
+  }
+
+-- | Transform the identifier type of 'StorageBackend' using an
+-- isomorphism.
+transformI :: Iso i' i -> StorageBackend a i e -> StorageBackend a i' e
+transformI iso (StorageBackend eventStore snapshotStore) =
+  StorageBackend (ES.transformI iso eventStore) (SS.transformI iso snapshotStore)
+
+-- | Transform the event type of 'StorageBackend' using an
+-- isomorphism.
+transformE :: Iso e' e -> StorageBackend a i e -> StorageBackend a i e'
+transformE iso (StorageBackend eventStore snapshotStore) =
+  StorageBackend (ES.transformE iso eventStore) snapshotStore
+
+-- | Transform the aggregate type of 'StorageBackend' using
+-- a prism.
+transformA :: (a' -> a, a -> Maybe a') -> StorageBackend a i e -> StorageBackend a' i e
+transformA prism (StorageBackend eventStore snapshotStore) =
+  StorageBackend eventStore (SS.transformA prism snapshotStore)
